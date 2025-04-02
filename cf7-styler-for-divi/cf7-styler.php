443ad7e26e83c9d4ead8491c5c124bba06619731
Sean Murphy <sean@iamseanmurphy.com>
2025-04-02 11:17:56 +0000

    2.3.0

--- cf7-styler-for-divi/cf7-styler.php
+++ cf7-styler-for-divi/cf7-styler.php
@@ -1,14 +1,14 @@
 <?php
 /*
-Plugin Name: Divi Form Styler
-Plugin URI: https://diviepic.com
+Plugin Name: Contact Form 7 Styler
+Plugin URI: https://diviextensions.com/divi-cf7-styler
 Description: Effortlessly style Contact Form 7, Gravity Forms, and Fluent Forms to match your site's design.
-Version: 2.2.1
-Author: DiviEpic
-Author URI:  https://diviepic.com/divi-form-styler
+Version: 2.3.0
+Author: DiviExtensions
+Author URI:  https://diviextensions.com
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: form-styler-for-divi
+Text Domain: cf7-styler-for-divi
 Domain Path: /languages
 */
 
@@ -17,32 +17,15 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('TFS_VERSION', '2.2.1');
-define('TFS_BASENAME', plugin_basename(__FILE__));
-define('TFS_BASENAME_DIR', plugin_basename(__DIR__));
-define('TFS_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('TFS_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('TFS_PLUGIN_ASSETS', trailingslashit(TFS_PLUGIN_URL . 'assets'));
-
-require_once 'plugin.php';
-
-add_action('wpcf7_admin_init', 'custom_tag_generator', 15);
-
-function custom_tag_generator()
-{
-    // Check if the tag generator class exists
-    if (class_exists('WPCF7_TagGenerator')) {
-        $tag_generator = WPCF7_TagGenerator::get_instance();
-        $tag_generator->add(
-            'custom_text',
-            __('Custom Text Field', 'contact-form-7'),
-            'jj'
-        );
-    }
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
 }
 
-// Function to define the custom tag form in the Contact Form 7 editor
-function custom_text_tag_generator($contact_form, $args = '')
-{
-    $args = wp_parse_args($args, array());
-}
+define('DCS_VERSION', '2.3.0');
+define('DCS_BASENAME', plugin_basename(__FILE__));
+define('DCS_BASENAME_DIR', plugin_basename(__DIR__));
+define('DCS_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('DCS_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('DCS_PLUGIN_ASSETS', trailingslashit(DCS_PLUGIN_URL . 'assets'));
+
+require_once 'includes/plugin.php';
